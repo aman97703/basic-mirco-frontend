c4e093eec0af89d8bfab3f688949fd1df022bad5
Aman Gupta <aman97703@gmail.com>
2025-06-06 19:29:25 +0530

    initial commit

---
+++ container/src/components/MarketingApp.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { mount as MarketingAppMount } from "marketing/MarketingApp";
+import { useRef } from "react";
+import { useEffect } from "react";
+
+const MarketingApp = () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      MarketingAppMount(ref.current);
+    }
+  }, [ref.current]);
+
+  return <div ref={ref} />;
+};
+
+export default MarketingApp;
